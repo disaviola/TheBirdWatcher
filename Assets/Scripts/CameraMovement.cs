7f09f466232aff7c22c2010026a2ab46bf68e42f
Disa Sahlström <disa7213@win.dsv.su.se>
2019-12-07 15:40:32 +0100

    Camera Movement Script

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private float dragSpeed = 3.5f;
+    private float xAxis;
+    private float yAxis;
+
+    [SerializeField] private bool invertX = false;
+    [SerializeField] private bool invertY = false;
+
+    private void LateUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float x = Input.GetAxis("Mouse X") * dragSpeed;
+            float y = -Input.GetAxis("Mouse Y") * dragSpeed;
+            if (invertX)
+            {
+                x = -Input.GetAxis("Mouse X") * dragSpeed;
+            }
+            if (invertY)
+            {
+                y = Input.GetAxis("Mouse Y") * dragSpeed;
+            }
+            transform.Rotate(new Vector3(y, x, 0));
+            xAxis = transform.rotation.eulerAngles.x;
+            yAxis = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(xAxis, yAxis, 0);
+        }
+    }
+
+
+}
